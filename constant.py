994a2ab198b457e9eb8c511c6cabccfcf286f73f
Student <student@twiki.(none)>
2017-06-11 22:47:56 -0400

    got the distance thing to work and almost the fixed bumper in command

--- constant.py
+++ constant.py
@@ -2,29 +2,44 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+from kobuki_msgs.msg import BumperEvent
+
+pub = rospy.Publisher('kobuki_command', Twist, queue_size=10)
+command = Twist()
+
+# this doesnt work right now becasue our "command" isnt 
+# in a while loop waiting for commands from the user.
+def bumperCallback(data):
+    global pub, command
+    if data.state == 1:
+	print("samesame")
+        command.linear.x = 0
+        command.linear.y = 0
+        command.angular.z = 0
+        pub.publish(command)
 
 def forward():
 	rospy.init_node('forward', anonymous=True)
-
-	pub = rospy.Publisher('kobuki_command', Twist, queue_size=10)
-	command = Twist()
+	rospy.Subscriber('/mobile_base/events/bumper', BumperEvent, bumperCallback)
 
 	while pub.get_num_connections() == 0:
 		#print ("zero connections")
 		pass
 
-	command.linear.x = 0.3
-	command.angular.z = 0.0
-	pub.publish(command)
-	rospy.sleep(2.0)
-	command.linear.x = -0.3
-	command.angular.z = 0.0
-	pub.publish(command)
-	rospy.sleep(2.0)
 	command.linear.x = 0.0
-	command.angular.z = 0.0
+	#im using command.linear.y as our "desired distance" variable that is passed in.
+	command.linear.y = 0.0
+	command.angular.z = 0.9
 	pub.publish(command)
-	rospy.sleep(2.0)
+#	rospy.sleep(2.0)
+#	command.linear.x = -0.3
+#	command.angular.z = 0.0
+#	pub.publish(command)
+#	rospy.sleep(2.0)
+#	command.linear.x = 0.0
+#	command.angular.z = 0.0
+#	pub.publish(command)
+#	rospy.sleep(2.0)
 
 if __name__== '__main__':
 	forward()
--- constant_command.py
+++ constant_command.py
@@ -4,9 +4,11 @@ import rospy
 from kobuki_msgs.msg import Led
 from kobuki_msgs.msg import ButtonEvent
 from kobuki_msgs.msg import BumperEvent
+from std_msgs.msg import Empty
 import math
 from geometry_msgs.msg import Twist
-
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
 
 pub = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=10)
 pub1 = rospy.Publisher('/mobile_base/commands/led1', Led, queue_size=10)
@@ -18,7 +20,45 @@ currentCommand.linear.x = 0.0
 currentCommand.angular.z = 0.0
 targetCommand = Twist()
 targetCommand.linear.x = 0.0
+targetCommand.linear.y = 0.0
 targetCommand.angular.z = 0.0
+
+def resetter():
+    	pub = rospy.Publisher('/mobile_base/commands/reset_odometry', Empty, 
+		queue_size=10)
+    	while pub.get_num_connections() == 0:
+        	pass
+    	pub.publish(Empty())
+
+def odomCallback(data):
+	global targetCommand, buttonGreen, pub
+ 	   # Convert quaternion to degree
+	q = [data.pose.pose.orientation.x,
+        	data.pose.pose.orientation.y,
+         	data.pose.pose.orientation.z,
+         	data.pose.pose.orientation.w]
+    	roll, pitch, yaw = euler_from_quaternion(q)
+    	# roll, pitch, and yaw are in radian
+    	degree = yaw * 180 / math.pi
+    	x = data.pose.pose.position.x
+    	y = data.pose.pose.position.y
+
+	if(targetCommand.linear.y > 0):
+    		if(x >= targetCommand.linear.y):
+			buttonGreen = False
+			targetCommand.linear.x = 0
+			pub.publish(targetCommand)
+			print("thisisx: " + str(x) + "thisislineary:" + str(targetCommand.linear.y))
+	elif(targetCommand.linear.y < 0):
+		if(x <= targetCommand.linear.y):
+			buttonGreen = False
+			targetCommand.linear.x = 0
+			pub.publish(targetCommand)
+			print("same?")	
+
+
+    	msg = "(%.6f,%.6f) at %.6f degree." % (x, y, degree)
+  	rospy.loginfo(msg)
 
 def updateCommand(data):
     global targetCommand
@@ -35,6 +75,7 @@ def bumperCallback(data):
     global pub1, led, buttonGreen, targetCommand
     if data.state == 1:
 	targetCommand.linear.x = 0
+	targetCommand.linear.y = 0
 	targetCommand.angular.z = 0
 	print("button: " + str(buttonGreen) + "ledBefore:" + str(led))
 	buttonGreen = False
@@ -49,6 +90,7 @@ def buttonCallback(data):
         	str = str + "Button 0 is "
 		if buttonGreen == True:
 			targetCommand.linear.x = 0
+			targetCommand.linear.y = 0
 			targetCommand.angular.z = 0
 			led.value = 3
 			buttonGreen = False
@@ -73,8 +115,11 @@ def constantCommand():
     rospy.Subscriber("kobuki_command", Twist, updateCommand)
     rospy.on_shutdown(cleanUp)
     rospy.Subscriber('/mobile_base/events/button', ButtonEvent, buttonCallback)
-    rospy.Subscriber('/mobile_base/events/bumper', BumperEvent, bumperCallback)
-
+#    rospy.Subscriber('/mobile_base/events/bumper', BumperEvent, bumperCallback)
+    rospy.Subscriber('/odom', Odometry, odomCallback)
+#    rospy.spin()
+    resetter()
+    print("same") 
     while pub.get_num_connections() == 0:
 	pass
 
@@ -86,14 +131,19 @@ def constantCommand():
     while not rospy.is_shutdown():
 	if buttonGreen == True:
 		pub.publish(targetCommand)
-		#print(str(targetCommand))
+		if (targetCommand.linear.x != 0.0):
+			print(str(targetCommand))
 	else:
 		tempCommand = targetCommand
 		tempCommand.linear.x = 0.0	
+		tempCommand.linear.y = 0.0
 		tempCommand.angular.z = 0.0
 		pub.publish(tempCommand)
-		#print(str(tempCommand))
+#		print(str(tempCommand))
 	rospy.sleep(0.1)
 
 if __name__ == '__main__':
-    constantCommand()
+# we will prob need to run this reset before we do anything.
+#	resetter()    
+#        odomExample()
+	constantCommand()
